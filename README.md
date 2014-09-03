ce9f3fd6e770864d5821cbae1a546208f0a4e96d
Thomas Foster <thomas.foster80@gmail.com>
2014-09-03 07:05:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a sample test of using fog to interact with AWS s3 and using s3 as a static web content delivery system
---
+++ s3uploader.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'fog'
+
+connection = Fog::Storage.new({
+  :provider                => 'AWS',
+  :aws_access_key_id       => '',
+  :aws_secret_access_key   => '' 
+  })
+
+
+sc = connection.directories.get('petergriffin1')
+#sct = connection.directories
+#sct = connection.files
+#connection.put_bucket_website(sc.key , 'index.html')
+test = connection.files(options = {directory: sc})
+puts "Changing permissions on #{test[0].key}"
+connection.put_object_acl(sc.key, test[0].key, 'public-read')
